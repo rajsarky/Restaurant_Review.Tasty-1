428725cf56833e4bfccf623419263b6d8076c890
kai <k.n.sarkar@icloud.com>
2024-01-02 09:41:52 +0000

    Version 1.7.2 [morning - update]

--- app/src/main/java/com/sarkar/tasty/adapters/PostAdapter.kt
+++ app/src/main/java/com/sarkar/tasty/adapters/PostAdapter.kt
@@ -58,6 +58,7 @@ class PostAdapter(var context: Context, var postList: ArrayList<Post>):
             holder.binding.ratingBar.rating = postList.get(position).rating!!
         } catch (e:Exception){
 
+
         }
         holder.binding.caption.text = postList.get(position).caption
         holder.binding.like.setOnClickListener{
